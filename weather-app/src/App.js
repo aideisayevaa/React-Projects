b1c9017672d7b86f6c54ac85734ee679efd526c6
aideisayevaa <aideisayeva3@gmail.com>
2022-05-31 12:27:21 +0400

    Weather App - hava haqqinda api elave edildi

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,7 +4,20 @@ import axios from 'axios';
 
 function App() {
 
-  
+  const key = "77b5b0a01fdb9d36910757d2fc5e8bde" //saytdan goturduyumuz api uzun olduguna gore deyisene menimsedirik
+
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=${key}&units=metric`); //response daxilinde axcios.get metodu cagirir, ne vaxt gelse o vaxt gosterir () daxiline ise hansi Api-ni isteyirkse onun linkini yaziriq
+        console.log(response); //yazdigimiz response-nin console-den gosterilmesi ucundu
+      } catch (error) { //eger Api olmasa xeta mesaji verir
+        console.error(error); //errorun consolede gosterilmesi
+      }
+    }
+    getApi() //yuxaridaki funksiyanin cagirilmasi
+  }, []) //buradaki arrayin icerisi bosdursa sadece app ilk baslayanda cagirir, eger icerisine bir deyisen ve ya hook adi yazanda o hook her cagirilanda isleyir
+
   return (
     <div className="App">
       isdjucuids
