fe7ef327a406637b2f124655ec38cea2a6e585e2
aideisayevaa <aideisayeva3@gmail.com>
2022-05-31 19:04:12 +0400

    Weather App - qismen tamamlandi

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import City from './City';
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
 
 
   //input daxilinde yazilanlari state daxilinde saxlamaliyiq ona gore de state yaradiriq
-  const [search, setSearch] = useState("") //daxil edilenler text olacagina gore "" yaziriq
+  const [search, setSearch] = useState() //daxil edilenler text olacagina gore "" yaziriq
 
   //istifadeciden bize lazim olan datalari aldiqdan sonra onlari bir state daxilinde saxlamaliyiq
   const [city, setCity] = useState()
@@ -16,7 +17,8 @@ function App() {
   useEffect(() => {
     async function getApi() {
       try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=${key}&units=metric`); //response daxilinde axcios.get metodu cagirir, ne vaxt gelse o vaxt gosterir () daxiline ise hansi Api-ni isteyirkse onun linkini yaziriq
+        //asagidaki kodda london sozunu silib evezine search deyisenini daxil edirik ki, istifadecinin daxil etdiyi seherin melumatlari gosterilsin
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}`); //response daxilinde axcios.get metodu cagirir, ne vaxt gelse o vaxt gosterir () daxiline ise hansi Api-ni isteyirkse onun linkini yaziriq
         console.log(response); //yazdigimiz response-nin console-den gosterilmesi ucundu
         setCity(response.data) //biz deyisiklik edeceyimiz ucun bunu setCity daxiilinde yazdiq ve respons icerisindeki consolede gosterilen data 8 - i deyiseceyimiz ucun response.data yazdiq yenii ki setCity daxiline ne deyismek isteyirikse onu yaziriq
       } catch (error) { //eger Api olmasa xeta mesaji verir
@@ -26,14 +28,16 @@ function App() {
     getApi() //yuxaridaki funksiyanin cagirilmasi
   }, []) //buradaki arrayin icerisi bosdursa sadece app ilk baslayanda cagirir, eger icerisine bir deyisen ve ya hook adi yazanda o hook her cagirilanda isleyir
 
-  console.log(search)
+ console.log(search)
 
   return (
-    <div className="App">
+    <div className="App mb-3 pt-0 w-{250px}">
       {/* istifadecidn melumatlari input vasitesile alacagiq*/}
-      <input onChange={(e) => { setSearch(e.target.value) /* burada evvelce console.log yazmisdiq indi setSearch olaraq deyisdirdik cunki inputdan gelen her deyeri setSearch ile deyisiklik edirik */ }} className='border-4 bg-slate-500' type="text" /> {/* input daxilindeki deyer her deyisende deyisiklik olmasini isteyirikse onChange yaziriq */}
+      <input onChange={(e) => { setSearch(e.target.value) /* burada evvelce console.log yazmisdiq indi setSearch olaraq deyisdirdik cunki inputdan gelen her deyeri setSearch ile deyisiklik edirik */ }}
+        className='border-4 bg-slate-500 ' type="text" /> {/* input daxilindeki deyer her deyisende deyisiklik olmasini isteyirikse onChange yaziriq */}
       {/* onChange={(e)=>{console.log(e.target.value)}} bele olduqda inputun daxiline yazdiqlarimizin hamisi e ile alinir ve deyeri target.value ile console-de yazilr deyerler her deyisende gosterir */}
 
+      <City city={city} />
     </div>
   );
 }
---
+++ weather-app/src/City.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const City = ({ city }) => {
+
+    // console.log(city)
+    return (
+        <div>
+            clslijdc
+            {/* <h1>{cityy.main.temp}</h1>
+            <h1>{cityy.name}</h1>
+            <h1>{cityy.weather[0].main}</h1> */}
+        </div>
+    )
+}
+
+export default City
