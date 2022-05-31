d965cc67264ddcf272bb277dd6ddb0a03e5bae1d
aideisayevaa <aideisayeva3@gmail.com>
2022-05-31 12:04:58 +0400

    Weather App - fayllar elave edildi

---
+++ weather-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      isdjucuids
+    </div>
+  );
+}
+
+export default App;
