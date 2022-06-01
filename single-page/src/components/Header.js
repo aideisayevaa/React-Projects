0828024afe2d8fd0249bff94c2e1cb492ba4abea
aideisayevaa <aideisayeva3@gmail.com>
2022-06-01 23:37:42 +0400

    Single Page - fayllar elave edildi

---
+++ single-page/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+
+    return (
+        <div>
+            Header
+        </div>
+    )
+}
+
+export default Header
