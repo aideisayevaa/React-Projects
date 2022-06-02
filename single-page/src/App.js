77e3dc004366800798d1cdc2142bf24f7ccd3693
aideisayevaa <aideisayeva3@gmail.com>
2022-06-02 11:31:36 +0400

    Single Page - Header elave edildi

--- single-page/src/App.js
+++ single-page/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header';
+import React from 'react';
 
 function App() {
   return (
--- single-page/src/components/Header.js
+++ single-page/src/components/Header.js
@@ -1,13 +1,71 @@
 import React from "react";
 import styled from "styled-components";
+import logo from './logo.png'
+
+const StyledHeader = styled.div`
+    height: 20vh;
+    background-color: #465775;
+    padding: 0 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .logo-container{
+        flex: 0.5;
+
+        img{
+            height: 20vh;
+        }
+    }
+
+    .menu-container{
+        flex: 1;
+    }
+
+    ul{
+        list-style: none;
+    }
+
+    ul li{
+        float: left;
+        color: white;
+    }
+
+    ul li:not(:first-child){
+        margin-left:20px;
+    }
+
+    nav{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    button {
+        background-color: #bce5bf;
+        padding: 5px 20px;
+        border: none;
+    }
+
+
+`
+
 
 const Header = () => {
 
-    return (
-        <div>
-            Header
+    return <StyledHeader>
+        <div className="logo-container">
+            <img src={logo} alt="logo" />
         </div>
-    )
+        <nav className="menu-container">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <button>Login</button>
+        </nav>
+    </StyledHeader>
 }
 
 export default Header
